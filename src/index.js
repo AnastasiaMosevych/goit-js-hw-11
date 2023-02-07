fcf6d71310654d7cb503a9e7b723c15e6a3b1eab
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-02-07 21:35:51 +0100

    completed task

---
+++ src/index.js
@@ -0,0 +1,120 @@
+import { formToJSON } from 'axios';
+import Notiflix from 'notiflix';
+
+import ImagesApi from './ImagesApi.js';
+import LoadMoreBtn from './components/LoadMoreBtn.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const searchFormEl = document.getElementById("search-form");
+const inputEl = document.querySelector("input");
+const loadMoreBtn = new LoadMoreBtn({
+    selector: ".load-more",
+    isHidden: true,
+});
+const imagesApi = new ImagesApi();
+const galleryEl = document.querySelector('.gallery');
+let gallery = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: "alt",
+        captionDelay: 250,
+        scrollZoom: false
+});
+
+galleryEl.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+};
+
+searchFormEl.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    imagesApi.searchQuery = inputEl.value.trim();
+    clearImagesDiv();
+    imagesApi.resetPage();
+    loadMoreBtn.show();
+    fetchImages();
+};
+
+function createMarkupForAllImages(images) {
+    let markupForAllImages = "";
+    for (let image of images) {
+        markupForAllImages += createImagesMarkup(image);
+    } 
+
+    return markupForAllImages;
+};
+
+function createImagesMarkup({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+    return `
+        <div class="photo-card">
+        <a class="gallery__item" href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes: ${likes}</b>
+                </p>
+                <p class="info-item">
+                    <b>Views: ${views}</b>
+                </p>
+                <p class="info-item">
+                    <b>Comments: ${comments}</b>
+                </p>
+                <p class="info-item">
+                    <b>Downloads: ${downloads}</b>
+                </p>
+            </div>
+        </div>
+    `
+};
+
+function clearImagesDiv() {
+    document.querySelector(".gallery").innerHTML = "";
+}
+
+function createImagesDiv(markup) {
+    document.querySelector(".gallery").insertAdjacentHTML("beforeend", markup);
+    gallery.refresh();
+}
+
+function onError(err) {
+    console.error(err);
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+};
+
+loadMoreBtn.button.addEventListener("click", fetchImages);
+
+
+async function fetchImages() {
+    loadMoreBtn.disable();
+    imagesApi.searchQuery = inputEl.value.trim();
+    try {
+        const images = await imagesApi.getImages();
+        let pageCount = imagesApi.totalHits / imagesApi.imagesPerPage;
+        if (images.length === 0) {
+            throw new Error("Error");
+        } else if (imagesApi.totalHits !== null && pageCount <= imagesApi.queryPage) {
+            Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+            loadMoreBtn.disable();
+        } else {
+            createImagesDiv(createMarkupForAllImages(images));
+            loadMoreBtn.enable();
+            imagesApi.incrementPage();
+        }
+        return images;
+    } catch (err) {
+        onError(err);
+    }
+};
+
+  
+
+    
+    
+
